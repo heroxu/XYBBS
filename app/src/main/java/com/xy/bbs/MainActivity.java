73243a2efcc0e3e6f722f71229c7b8ccb7916ee7
mesmerize <5060265@qq.com>
2016-11-01 17:07:49 +0800

    This is my first Github

--- app/src/main/java/com/xy/bbs/MainActivity.java
+++ app/src/main/java/com/xy/bbs/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Toast.makeText(this,"xxr",Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this,"yyp",Toast.LENGTH_SHORT).show();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
