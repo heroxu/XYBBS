cef954797468879da81031c96bafdf0cb2a03027
mesmerize <5060265@qq.com>
2016-11-05 20:40:35 +0800

    This is my first Github

--- app/src/main/java/com/xy/bbs/MainActivity.java
+++ app/src/main/java/com/xy/bbs/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Toast.makeText(this,"我草。。。",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"我草。啊。。",Toast.LENGTH_SHORT).show();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
