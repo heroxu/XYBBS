e9a3d6dc2870e2bab2b53df3f50df6d2259930e8
mesmerize <5060265@qq.com>
2016-11-05 20:54:55 +0800

    This is my first Github

---
+++ app/src/main/java/com/xy/bbs/Test.java
@@ -0,0 +1,13 @@
+package com.xy.bbs;
+
+/**
+ * Created by mesmerize on 2016/11/5.
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("args = " + args);
+    }
+}
