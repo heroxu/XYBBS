ba2b5c0204f9ee97507699b46e0939c71a611abc
mesmerize <5060265@qq.com>
2016-11-05 21:23:38 +0800

    全部删除  从零开始

--- app/src/main/java/com/xy/bbs/MainActivity.java
+++ app/src/main/java/com/xy/bbs/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toast.makeText(this,"全删除了  哈哈哈",Toast.LENGTH_SHORT).show();
 
     }
