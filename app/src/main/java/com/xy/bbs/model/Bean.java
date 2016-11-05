9989a93d01b0edab201948e46925073931d431c7
mesmerize <5060265@qq.com>
2016-11-05 21:18:01 +0800

    哈哈哈  这次终于成功了

---
+++ app/src/main/java/com/xy/bbs/model/Bean.java
@@ -0,0 +1,13 @@
+package com.xy.bbs.model;
+
+/**
+ * Created by mesmerize on 2016/11/5.
+ */
+
+public class Bean {
+
+    public static void main(String[] args) {
+
+        System.out.println("args = " + args);
+    }
+}
